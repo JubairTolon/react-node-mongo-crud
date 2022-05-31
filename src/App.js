223974c77f77450a5c3b770036492b68c659c72d
ZubayerAhammedTolon <zs.tolon0073@gmail.com>
2022-05-31 15:05:15 +0600

    create components for insert data to mongo server

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import AddUser from './Components/AddUser/AddUser'
+import Home from "./Components/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="user/add" element={<AddUser></AddUser>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
